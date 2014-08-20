4097103b734e34603d4ae57a8b8a2ec401733279
ccalonzo <ccalonzo@gmail.com>
2014-08-20 07:15:28 -0400

    Fixed: cylinder can be at arbitrary z

--- scratch/draw3dCylinder.m
+++ scratch/draw3dCylinder.m
@@ -1,8 +1,8 @@
-function Sketch = draw3dCylinder(x1,y1,radius,phiDegrees,thetaDegrees,PreviousSpace)
+function Sketch = draw3dCylinder(x1,y1,z1,radius,phiDegrees,thetaDegrees,PreviousSpace)
 % Draws a 3D cylindrical volume in PreviousSpace. PreviousSpace may already
 % contain other elements, but should have equal dimensions.
-% Specify (x1,y1) as the intersection of the cylinder axis with the first
-% plane of PreviousSpace; radius of the cylinder; zentih angle, phiDegrees; 
+% Specify a point (x1,y1,z1) on the cylinder axis with reference to the 
+% axes of PreviousSpace; radius of the cylinder; zentih angle, phiDegrees; 
 % and azimuth angle, thetaDegrees. Angles are measured from the positive z
 % and x axes, respectively.
 % This function requires drawEllipse.m
@@ -15,8 +15,8 @@ majorAxis = radius/cos(phi);
 Sketch = zeros(size(PreviousSpace));
 
 for k = 1:size(PreviousSpace,3)
-    xk = cos(theta)*(double(k)*tan(phi));
-    yk = sin(theta)*(double(k)*tan(phi)) + 1;
+    xk = cos(theta)*(double(k-z1)*tan(phi));
+    yk = sin(theta)*(double(k-z1)*tan(phi)) + 1;
     Sketch(:,:,k) = drawEllipse(x1+xk,y1+yk,majorAxis,radius,thetaDegrees,PreviousSpace(:,:,k));
 end
 
--- scratch/exampleFibers.m
+++ scratch/exampleFibers.m
@@ -4,11 +4,11 @@
 
 Slate = zeros(256,256,256);
 
-Sketch = draw3dCylinder(1,1,8,45,45,Slate);
-Sketch = draw3dCylinder(1,128,16,90,0,Sketch);
-Sketch = draw3dCylinder(200,20,16,60,120,Sketch);
-Sketch = draw3dCylinder(225,225,32,0,0,Sketch);
-Sketch = draw3dCylinder(1,200,8,30,-30,Sketch);
+Sketch = draw3dCylinder(1,1,1,8,45,45,Slate);
+Sketch = draw3dCylinder(1,128,64,16,90,0,Sketch);
+Sketch = draw3dCylinder(200,20,1,16,60,120,Sketch);
+Sketch = draw3dCylinder(225,225,1,32,0,0,Sketch);
+Sketch = draw3dCylinder(1,200,1,4,30,-30,Sketch);
 
 implay(Sketch);
 
